85d45967d419b23fa28ded746d6f4aa0a5896232
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-09 13:39:16 +0600

    documentation updated

--- assignment.js
+++ assignment.js
@@ -6,25 +6,25 @@ Input: 120 (in sher)
 Output: 3 (in mon)
 */
 
-//Problem-1: Function that convert sher to mon
+/* Problem-1: Function that convert sher to mon */
 function seerToMon(mySeer){
-    //Input validation or error handling. if the input is NOT a NUMBER or a NEGATIVE NUMBER! Weight cannot be Negative!
+    /* Input validation or error handling. if the input is NOT a NUMBER or a NEGATIVE NUMBER! Weight cannot be Negative! */
     if(typeof(mySeer) != 'number' || mySeer < 0){
         return "WRONG INPUT! Please Input a Positive Weight! Negative Weight or Strings are NOT ALLOWED";
     }
 
-    //Main calculation to convert the sher into mon and return the value!
+    /* Main calculation to convert the sher into mon and return the value as mon! */
     else{
         let mon = mySeer / 40;
         return mon;
     }
 }
 
-//Check the result by printing the output outside the function
-let myMon = seerToMon(0);
+/* Check the result by printing the output outside the function */
+let myMon = seerToMon(240);
 console.log(myMon);
 
-// *************************Problem-1 Finished*************************
+/* *************************Problem-1 Finished************************* */
 
 
 /*
@@ -45,19 +45,19 @@ Output: totalSalesAmount = (shirtUnitPrice * shirtQuantity) + (pantUnitPrice * p
 totalSalesAmount = 2700
 */
 
-//Problem-2: Function to calculate the total sales cost of a shop!
+/* Problem-2: Function to calculate the total sales cost of a shop! */
 function totalSales(shirtQuantity, pantQuantity, shoeQuantity){
     //pre-defined unit price
     const shirtUnitPrice = 100;
     const pantUnitPrice = 200;
     const shoeUnitPrice = 500;
 
-    //Input validation or error handling. if there is any input value is NOT a NUMBER or Fractional Value! Negative number is allowed, because sometimes customers return products!
+    /* Input validation or error handling. if there is any input value is NOT a NUMBER or Fractional Value! Negative number is allowed, because sometimes customers return products! */
     if(typeof(shirtQuantity) != 'number' || typeof(pantQuantity) != 'number' || typeof(shoeQuantity) != 'number' || Number.isInteger(shirtQuantity) == false || Number.isInteger(pantQuantity) == false || Number.isInteger(shoeQuantity) == false) {
         return "WRONG INPUT! Please input the quantities as INTEGERs! Shirt, pant or shoe quantity cannot have a FRACTIONAL or STRING value!";
     }
 
-    //Main calculation to calculate the Total Sales Amount of all products!
+    /* Main calculation to calculate the Total Sales Amount of all products! */
     else{
         let shirtTotalSales = shirtUnitPrice * shirtQuantity;
         let pantTotalSales = pantUnitPrice * pantQuantity;
@@ -65,12 +65,12 @@ function totalSales(shirtQuantity, pantQuantity, shoeQuantity){
 
         let totalSalesAmount = shirtTotalSales + pantTotalSales + shoeTotalSales;
 
-        //Return the desired value as number!
+        /* Return the desired value as number! */
         return totalSalesAmount;
     }
 }
 
-//Check the result by printing the output outside the function
+/* Check the result by printing the output outside the function */
 let myTotalSalesAmount = totalSales(5, 8, 3);
 console.log(myTotalSalesAmount);
 
@@ -92,20 +92,20 @@ Output: totalDeliveryCost (in number)
 
 */
 
-//Problem-3: Function to calculate the total delivery cost based on T-Shirt Quantity! 
+/* Problem-3: Function to calculate the total delivery cost based on T-Shirt Quantity!  */
 function deliveryCost(tshirtQuantity){
-    //Input validation! if there is any input value is NOT a NUMBER or Fractional Value!
+    /* Input validation! if there is any input value is NOT a NUMBER or Fractional Value! */
     if(typeof(tshirtQuantity) != 'number' || tshirtQuantity < 0 || Number.isInteger(tshirtQuantity) == false){
         return "WRONG INPUT! Please input the quantity as a POSITIVE INTEGER! T-Shirt quantity cannot have a FRACTIONAL value, or NEGATIVE value, or STRING!";
     }
     
-    //Total Delivery Cost if the T-Shirt Quanity is 0 to 100!
+    /* Total Delivery Cost if the T-Shirt Quanity is 0 to 100! */
     else if(tshirtQuantity >= 0 && tshirtQuantity <= 100){
         let totalDeliveryCost = tshirtQuantity * 100;
         return totalDeliveryCost;
     }
 
-    //Total Delivery Cost if the T-Shirt Quanity is 100 to 200!
+    /* Total Delivery Cost if the T-Shirt Quanity is 100 to 200! */
     else if(tshirtQuantity > 100 && tshirtQuantity <=200){
         let costForFirstLot = 100 * 100;
         let costForSecondLot = (tshirtQuantity - 100) * 80;
@@ -113,7 +113,7 @@ function deliveryCost(tshirtQuantity){
         return totalDeliveryCost;
     }
 
-    //Total Delivery Cost if the T-Shirt Quanity is more than 200!
+    /* Total Delivery Cost if the T-Shirt Quanity is more than 200! */
     else{
         let costForFirstLot = 100 * 100;
         let costForSecondLot = 100 * 80;
@@ -124,12 +124,12 @@ function deliveryCost(tshirtQuantity){
     
 }
 
-//Check the result by printing the output outside the function!
+/* Check the result by printing the output outside the function! */
 let myTotalDeliveryCost = deliveryCost(160);
 console.log(myTotalDeliveryCost);
 
 
-// *************************Problem-3 Finished*************************
+/* *************************Problem-3 Finished************************* */
 
 
 
@@ -142,42 +142,42 @@ Output: "ozair"
 */
 
 
-//Problem-4: Function to find the first name in an array with exact length = 5!
+/* Problem-4: Function to find the first name in an array with exact length = 5! */
 function perfectFriend(friendsNameArr){
-    //define the variable to return the first perfect name. If there is no perfect match (name length = 5) found in the array, it will retrun the string "SORRY! There is NO NAME with exact lenght = 5!" 
+    /* define the variable to return the first perfect name. If there is no perfect match (name length = 5) found in the array, it will retrun the string "SORRY! There is NO NAME with exact lenght = 5!"  */
     let perfectName = "SORRY! There is NO NAME with exact lenght = 5!";
     
-    //Input validation. If the funtion parameter contains something other than an Array!
+    /* Input validation. If the funtion parameter contains something other than an Array! */
     if(Array.isArray(friendsNameArr) == false){
         return "WRONG INPUT! Please input an ARRAY!";
     }
 
     else{
-        //Input validation. If the array items contains anything other than string!
+        /* Input validation. If the array items contains anything other than string! */
         for(const name of friendsNameArr){
             if(typeof(name) != 'string'){
                 return "WRONG INPUT! NO NUMBER ALLOWED! Please input all the names as STRING!";
             }
         }
     
-        //Core calculation. This block of code finds the first name with exact length = 5, and return the name as STRING!
+        /* Core calculation. This block of code finds the first name with exact length = 5, and return the name as STRING! */
         for(const name of friendsNameArr){
             if(name.length == 5){
                 perfectName = name;
                 break;
             }
         }
-        //return the required value as string
+        /* return the required value as string */
         return perfectName;
     }
     
 }
 
-//Declare an array containing my friends' name!
+/* Declare an array containing my friends' name! */
 let myFriendsName = ['sami', 'Farmee', 'Arishamma', 'Arashabba', 'tasnuva', 'Ozair Alam', 'Rasel', 'Hasan', 'Hussain Rubel', 'Rajib', 'Shipul', 'Wahid'];
 let myPerfectFriend = perfectFriend(myFriendsName);
 
-//Check the result by printing the output outside the function!
+/* Check the result by printing the output outside the function! */
 console.log(myPerfectFriend);
 
-// *************************Problem-4 Finished*************************
+/* *************************Problem-4 Finished************************* */
